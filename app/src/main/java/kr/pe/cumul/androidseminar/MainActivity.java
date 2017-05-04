8bdbfeef5c19ccd699d8f8459d81273fdc66a92f
cumul <gg6123@naver.com>
2017-05-05 03:27:31 +0900

    4주차 - Menu 추가하기

--- app/src/main/java/kr/pe/cumul/androidseminar/MainActivity.java
+++ app/src/main/java/kr/pe/cumul/androidseminar/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,5 +38,14 @@ public class MainActivity extends AppCompatActivity {
         adapter.add(new Todo("자료구조 과제", false));
         adapter.add(new Todo("윤리학 쪽글", false));
         adapter.add(new Todo("논리학 연습문제", true));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem add = menu.add("추가");
+        add.setIcon(R.drawable.ic_add_white_24dp);
+        add.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+
+        return true;
     }
 }
